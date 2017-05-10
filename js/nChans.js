a324f5f32df1a52a3efea0f250f9b9740bcf2c39
HolyPancakes <marubaul22@gmail.com>
2017-05-10 13:18:47 +0800

    Initial JS NChans with GUI

---
+++ js/nChans.js
@@ -0,0 +1,59 @@
+(function() {
+	'use strict';
+
+
+	// ---------------------------------------------------------------------//
+    //                               CONSTANTS                              //
+    // ---------------------------------------------------------------------//
+
+ 	// ---------------------------------------------------------------------//
+    //                               ANIMATION                              //
+    // ---------------------------------------------------------------------//
+
+    // ---------------------------------------------------------------------//
+    //                               UTILITIES                              //
+    // ---------------------------------------------------------------------//
+
+	function getMD(i,j,k,l){
+		return Math.abs(i-k)+Math.abs(j-l);
+	}
+
+	function isSafe(candidate, optionsTOS, n){
+		var i=(candidate-1)/n+1, 
+			j=(candidate-1)%n+1, 
+			k=(optionTOS-1)/n+1, 
+			l=(optionTOS-1)%n+1;
+		return getMD(i,j,k,l)!=3 && i!=k && j!=l;
+	}
+
+	function isPuzzleMove(x, puzzleMoves){
+		return puzzleMoves.includes(x);
+	}
+
+	function init(){
+
+	}
+
+	function solveNChans(){
+
+	}
+
+	//test
+	var cfg = {
+		draggable: true,
+    	dropOffBoard: 'trash',
+    	sparePieces: true,
+    	showNotation: false,
+    	onChange: function(oldPos, newPos) {
+	    	document.getElementById("fen").innerHTML = ChessBoard3.objToFen(newPos);
+	    }
+   	};
+	var board1 = new ChessBoard3('board', cfg);
+
+
+	// ---------------------------------------------------------------------//
+    //                           WIDGET DEFINITION                          //
+    // ---------------------------------------------------------------------//
+
+
+}) ();
